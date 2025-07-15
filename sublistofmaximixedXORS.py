a25515abaadeb0f0fe93f24f07fd0627c5f0afa0
syedmhamil <syedmahamil56@gmail.com>
2025-07-15 10:40:27 +0500

    hormax

---
+++ sublistofmaximixedXORS.py
@@ -0,0 +1,14 @@
+# list_xors = [[3,4,5],[5,6,7],[45,54,23]]
+# Which_max= []
+# for listX in list_xors:
+#     xor_val = 0 
+#     for v in listX:
+#         xor_val^=v
+
+#     Which_max.append({xor_val})
+
+# p= max(Which_max)
+# print(p)
+
+
+# If you also want to know which sublist gave that max XOR, let me know — that's the only thing you haven't tracked yet.
---
+++ zero-shifer-concept.py
@@ -0,0 +1,16 @@
+# nums=[0, 1, 0, 0, 0, 3, 0, 12]
+# newA= []    
+# count=0
+# for n in range(len(nums)):
+#     if nums[n]==0:
+
+#        newA.append(nums[n])
+#     else:
+#         newA.insert(count,nums[n])
+#         count+=1
+
+# print(newA)
+
+
+
+
