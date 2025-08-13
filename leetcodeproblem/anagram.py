8e135655b2bcd9c54aa7eaea65f219c138bfaa5d
syedmhamil <syedmahamil56@gmail.com>
2025-08-13 12:43:24 +0500

    ana

---
+++ leetcodeproblem/anagram.py
@@ -0,0 +1,28 @@
+# ============================================
+#          Brute Force ---> method
+# ============================================
+
+
+# def checkAnagram(s,t):
+#     # Input: s = "racecar", t = "carrace"
+#     sorted_s = sorted(s)
+#     sorted_t= sorted(t)
+#     return  sorted_s == sorted_t
+
+# print(checkAnagram('abc','cha'))
+
+       
+
+
+# ============================================
+#          hash method ---> method
+# ============================================
+
+def checkAnagramSet(s,j):
+    if len(s) != len(j):
+       False
+    # Input: s = "racecar", t = "carrace"
+    count_S,Count_T = {},{}
+    
+    
+
