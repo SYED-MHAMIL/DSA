5e28d5afc8e323e387d8fd13521a398cdf5e349c
syedmhamil <syedmahamil56@gmail.com>
2025-08-14 23:15:21 +0500

    dsa

--- leetcodeproblem/anagram.py
+++ leetcodeproblem/anagram.py
@@ -1,3 +1,5 @@
+from operator import add
+import string 
 # ============================================
 #          Brute Force ---> method
 # ============================================
@@ -15,14 +17,54 @@
 
 
 # ============================================
-#          hash method ---> method
+#          like hash method ---> method
 # ============================================
+from functools import reduce
 
-def checkAnagramSet(s,j):
-    if len(s) != len(j):
-       False
+def checkAnagramSet(s,t):
+    if len(s) != len(t):
+       return False
     # Input: s = "racecar", t = "carrace"
-    count_S,Count_T = {},{}
-    
-    
+
+    k= string.ascii_lowercase
+    p={i:0 for i in k}
+   
+  
+    for ch in s:
+        if ch in p:
+            p[ch] += 1
+    for ch in t:
+        if ch in p:
+              p[ch] -= 1
+    print('\n after',p)
+    return reduce(lambda acc,ch:acc+abs(ch),p.values()) ==0 
+# print(checkAnagramSet('asc','cba'))
+
+
+
+
+
+# ============================= =========================
+#           hash map method ---> method
+# =================================================
+
+
+
+# def checkAnagram(s,t):
+#    if len(s) != len(t):
+#       return False   
+#    count_S, count_T = {}, {}
+#    # s= aad, t= daa
+#    for i in range(len(s)):
+#       count_S[s[i]] = 1 + count_S.get(s[i],0)
+#       count_T[t[i]] =1 +count_T.get(t[i],0) 
+#    print('firsts', count_S)
+#    print('Second count', count_T)
+
+#    return   count_S == count_T
+
+# print(checkAnagram('aad','daa'))
+
+
+
 
