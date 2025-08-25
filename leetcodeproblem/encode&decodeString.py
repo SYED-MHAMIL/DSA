ac1d11b01a1861d4f4f378986e543bd8210b42bd
syedmhamil <syedmahamil56@gmail.com>
2025-08-25 10:23:04 +0500

    encode&decode

--- leetcodeproblem/encode&decodeString.py
+++ leetcodeproblem/encode&decodeString.py
@@ -12,7 +12,7 @@ def encode(str_arr):
         res+= str(len(arr)) + "#" + arr
     return res
     
-print(encode(arr1))         
+# print(encode(arr1))         
     
 def decode(str_arr):
     # 4#need5#c#ode
@@ -25,10 +25,39 @@ def decode(str_arr):
         res.append(str_arr[j+1 : j+1+ length ])
         i = j+1+ length  
     return res
-print(decode('4#need5#c#ode'))
+# print(decode('4#need5#c#ode'))
 
 
 
+# practise  
 
 
+def encode_str(arr):
+    res = ''
+    for stri in arr:
+        res+=(str(len(stri))+'#'+stri)
+    return res
+print(encode_str(arr1))    
 
+
+def decode_str(arr_str):
+    res,i = [], 0
+    # arr= 4#need5#c#ode
+    while i < len(arr_str):
+        j =  i 
+        while arr_str[j] != "#":
+              j+=1
+        length = int(arr_str[i:j])
+        res.append(arr_str[j+1:j+1+length])
+        i= j+1+length
+    return res
+
+print(decode_str('4#need5#c#ode'))
+
+
+        
+        
+
+
+         
+         
