d9f911add8d8b8ac1eae477bf58e7497845ad834
syedmhamil <syedmahamil56@gmail.com>
2025-09-15 11:49:02 +0500

    'dsa

--- leetcodeproblem/max_rotated_sorted.py
+++ leetcodeproblem/max_rotated_sorted.py
@@ -33,4 +33,57 @@ def findMin( nums):
     return res 
 
  
-print(findMin([3,4,5,6,1,2]))
+print(findMin([4,5,0,1,2,3]))
+
+
+# yoiu can also check from right side 
+
+def findMin( nums):
+        # Input: nums = [3,4,5,6,1,2]
+    print("check from the right pointer ")                        #m 
+    res = nums[0]
+    l =  0
+    r = len(nums) -1  
+    
+    while l <= r:
+        if nums[l] < nums[r]:
+           res = min(res,nums[l])
+
+           break
+        # print("bahir") 
+        m = (l+r) // 2
+        res= min(res,nums[m])    
+        if nums[m] >= nums[r]:
+            l=m+1
+        else:
+            r= m-1
+             
+    return res 
+
+ 
+print(findMin([4,5,0,1,2,3]))
+
+
+
+def findMinOther(nums):
+    print("printed")
+    l = 0 
+    r = len(nums)  -1
+    res = nums[0] 
+    while l <= r:
+      if nums[l] < nums[r]:
+        res = min(res,nums[l])
+        break
+    #   [4,5,0,1,2,3]
+      mid = (l+r)//2
+      res = min(res,nums[mid])
+      print("min", res)
+      if nums[mid] >= nums[l]:
+          l= mid+1
+      else:
+          r= mid -1
+    return res
+
+          
+print(findMinOther([4,5,0,1,2,3]))
+
