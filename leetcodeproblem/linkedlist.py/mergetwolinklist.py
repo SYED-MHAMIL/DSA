18b282ecf5ef9cfd3964fea507d998edabbdc6dd
syedmhamil <syedmahamil56@gmail.com>
2025-09-22 11:18:27 +0500

    'dsa'

--- leetcodeproblem/linkedlist.py/mergetwolinklist.py
+++ leetcodeproblem/linkedlist.py/mergetwolinklist.py
@@ -34,6 +34,7 @@ def mergedWithIteration(head1,head2):
           head2.next = preverseHead1   
         #   1--1---2 
           head1 = preverseHead1
+          
           preverseHead1 = preverseHead1.next
           head2 = temp
           head1.next = head2
@@ -82,8 +83,48 @@ head2.next = Node(3)
 head2.next.next = Node(5)    
 head2.next.next.next = Node(7)    
 
-reversehead = mergedWithIteration(head,head2)
+# reversehead = mergedWithIteration(head,head2)
+# printList(reversehead)
+
+
+
+
+
+
+
+def mergedWithIteration2(head1,head2):
+    head = head1 
+    preserveHead1 = head1.next
+    
+# Input: list1 = [1,2,4], list2 = [1,3,5]
+# Output: [1,1,2,3,4,5] 
+    while preserveHead1 and head2:
+        head1.next= head2  #link
+        # 1--1--3--5 
+        # first preserve the list 3,5
+        saveHead2List = head2.next
+        head2.next = preserveHead1
+        # 1--1--2
+        head1 = preserveHead1
+        preserveHead1= preserveHead1.next 
+        head2 =  saveHead2List
+
+
+        head1.next = head2
+        # 1--1--2--3
+
+        # 
+
+        return head
+
+
+reversehead = mergedWithIteration2(head,head2)
 printList(reversehead)
 
 
 
+
+
+#*******************************************************************************
+                    # POINTER Method
+#*******************************************************************************
