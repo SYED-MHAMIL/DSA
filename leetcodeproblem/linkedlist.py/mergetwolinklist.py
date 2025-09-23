7cc6e9a38531afbb20cc6d4a07d667842cab197a
syedmhamil <syedmahamil56@gmail.com>
2025-09-23 10:53:22 +0500

    'dsa'

--- leetcodeproblem/linkedlist.py/mergetwolinklist.py
+++ leetcodeproblem/linkedlist.py/mergetwolinklist.py
@@ -16,7 +16,7 @@
 
 
 # ********************************************************************************
-                    #        Iteration 
+                    #        Iteration with pointer
 # ********************************************************************************
 
 
@@ -118,13 +118,97 @@ def mergedWithIteration2(head1,head2):
         return head
 
 
-reversehead = mergedWithIteration2(head,head2)
-printList(reversehead)
+
+
+
+
+
+
+# reversehead = mergedWithIteration2(head,head2)
+# printList(reversehead)
+
+
+
+def mergedWithIteration3(head1,head2):
+     head = head1
+     preserveHead1Next = head1.next 
+     
+     # Input: list1 = [1,2,4,6], list2 = [1,3,5,7]
+     # Output: [1,1,2,3,4,5] 
+     while preserveHead1Next and head2:
+           head1.next = head2 
+           head2Preserve = head2.next
+           head2.next = preserveHead1Next
+        #    1--1--2
+           head1 = preserveHead1Next
+           preserveHead1Next = preserveHead1Next.next   
+           head2 = head2Preserve 
+           head1.next = head2
+           
+
+     return head            
+
+     
+
+# reversehead = mergedWithIteration3(head,head2)
+# printList(reversehead)
+
+
+
+
+
+# *************************************************************
+    #            ITERATION BUT READABLE
+# *************************************************************
+
+
+#  Iteration
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, data=0, next=None):
+        self.data = data
+        self.next = next
+
+def mergedWithIteration4(head1,head2):
+    dummy = node  =  ListNode()
+#   for example:  list1= [1,2,4] , list2 =[1,3,5]
+    while head1 and head2:
+        if head1.data < head2.data:
+            node.next = head1
+            head1= head1.next 
+        else:
+            node.next = head2
+            head2= head2.next 
+        node = node.next   
+    node.next = head1 or head2 
+    return dummy.next
+
+    
+           
+
+     
+
+mergedHead= mergedWithIteration4(head,head2)
+printList(mergedHead)
+
 
 
 
 
 
 #*******************************************************************************
-                    # POINTER Method
+                    #      RECURSION   METHOD
 #*******************************************************************************
+
+print("""
+             ***********    with the help of Pointer method    ***********
+""")
+
+
+
+def mergedWithRecursion(head1,head2):
+    pass
+     
+
+# mergedHead = mergedWithRecursion(head,head2)
+# printList(mergedHead)
