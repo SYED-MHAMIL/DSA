f1226247f9818f8b21935eb1dbebf9911b02af31
syedmhamil <syedmahamil56@gmail.com>
2025-09-25 10:07:28 +0500

    dsa

--- leetcodeproblem/linkedlist.py/mergetwolinklist.py
+++ leetcodeproblem/linkedlist.py/mergetwolinklist.py
@@ -73,14 +73,14 @@ class Node:
 
 head = Node(1)    
 head.next = Node(2)    
-head.next.next = Node(4)    
+# head.next.next = Node(4)    
 # head.next.next.next = Node(6)    
 
 # for  head 2
 
 head2 = Node(1)    
 head2.next = Node(3)    
-head2.next.next = Node(5)    
+# head2.next.next = Node(5)    
 # head2.next.next.next = Node(7)    
 
 # reversehead = mergedWithIteration(head,head2)
@@ -207,32 +207,29 @@ print("""
 
 
 def mergedWithRecursion(head1,head2):
-    # class ListNode:
-    #    def __init__(self, data=0, next=None):
-    #     self.data = data
-    #     self.next = next
-    
     # Input: list1 = [1,2,4], list2 = [1,3,5]
     # Output: [1,1,2,3,4,5]
 
-    if head1 is None:
+    if head1 is None:                                                  
         return head2
     if head2 is None:
          return head1
     
     if head1.data <= head2.data:
-       head1.next= mergedWithRecursion(head1.next,head2)
-       print("head1",head1.data)
-       print("head1.next",head1.next.data)
-       return head1 
+       head1.next = mergedWithRecursion(head1.next,head2)
+       return head1
     else:
-        head2.next= mergedWithRecursion(head1,head2.next)
-        print("head2",head1.data)
-        print("head2.next",head2.next.data)
-
-        return head2  
-    
+        head2.next = mergedWithRecursion(head1,head2.next)
+        return head2
 
 
+
+# 1,1,2,3,4,5
+# 5
 mergedHead = mergedWithRecursion(head,head2)
 printList(mergedHead)
+
+
+
+
+
