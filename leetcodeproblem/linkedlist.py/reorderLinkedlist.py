2564dc240edf48a5851fce118819301fa4262da9
syedmhamil <syedmahamil56@gmail.com>
2025-10-07 11:24:32 +0500

    dsa

--- leetcodeproblem/linkedlist.py/reorderLinkedlist.py
+++ leetcodeproblem/linkedlist.py/reorderLinkedlist.py
@@ -44,12 +44,12 @@ class Node:
 
 # for head1
 
-head = Node(2)    
-head.next = Node(4)    
-head.next.next = Node(6)    
-head.next.next.next = Node(8)    
-# head.next.next.next.next = Node(5)    
-# head.next.next.next.next.next = Node(6)
+head = Node(1)    
+head.next = Node(2)    
+head.next.next = Node(3)    
+head.next.next.next = Node(4)    
+head.next.next.next.next = Node(5)    
+head.next.next.next.next.next = Node(6)
 
 
 
