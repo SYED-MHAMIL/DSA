c7d06cb2340b6cd6af59e51107b82aa2074c0c29
syedmhamil <syedmahamil56@gmail.com>
2025-10-15 11:02:15 +0500

    remove-node

--- leetcodeproblem/linkedlist.py/removeNode.py
+++ leetcodeproblem/linkedlist.py/removeNode.py
@@ -1,5 +1,18 @@
 ################################################## 
 #                BRUTE FORCE APPROACH
 ################################################## 
- 
 
+
+
+
+class Node:
+    def __init__(self,data):
+        self.data = data 
+        self.next = None
+    
+# for head1
+
+head = Node(2)    
+head.next = Node(4)    
+head.next.next = Node(6)    
+head.next.next.next = Node(8)  
