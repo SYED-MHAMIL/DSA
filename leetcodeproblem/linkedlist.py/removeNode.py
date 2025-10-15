6f596c2757c1d967436f126546bffce8553e3d06
syedmhamil <syedmahamil56@gmail.com>
2025-10-15 11:06:12 +0500

    remove-node

--- leetcodeproblem/linkedlist.py/removeNode.py
+++ leetcodeproblem/linkedlist.py/removeNode.py
@@ -5,6 +5,18 @@
 
 
 
+# You are given the beginning of a linked list head, and an integer n.
+
+# Remove the nth node from the end of the list and return the beginning of the list.
+
+# Example 1:
+
+# Input: head = [1,2,3,4], n = 2
+# 4-2 
+# 1-1 =>0
+# Output: [1,2,4]
+
+
 class Node:
     def __init__(self,data):
         self.data = data 
@@ -12,7 +24,12 @@ class Node:
     
 # for head1
 
-head = Node(2)    
-head.next = Node(4)    
-head.next.next = Node(6)    
-head.next.next.next = Node(8)  
+head = Node(1)    
+head.next = Node(2)    
+head.next.next = Node(3)    
+head.next.next.next = Node(4)  
+
+
+
+arr = []
+
