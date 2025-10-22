4d185e289a37755ce19eb11c9d012ff435130fb3
syedmhamil <syedmahamil56@gmail.com>
2025-10-22 12:52:09 +0500

    dsa

--- leetcodeproblem/linkedlist.py/remove-midd.py
+++ leetcodeproblem/linkedlist.py/remove-midd.py
@@ -34,19 +34,37 @@ class Node:
 
 head = Node(1)    
 head.next = Node(2)    
-head.next.next = Node(3)    
-head.next.next.next = Node(4)  
-head.next.next.next.next = Node(5)  
+# head.next.next = Node(3)    
+# head.next.next.next = Node(4)  
+# head.next.next.next.next = Node(5)  
 
 def removeMid(header):
-    left = header
-    rihgt = header.next 
-    prev= header
-    
-    if not header.next:
+    if not header.next or header:
         return None
     
-    
+    left = header
+    right = header 
+    prev= None
+    # 1 , 2 
+    # prev = 1
+    # left = 2
+    #  right  = 3
+    while right and right.next:
+        prev = left
+        left= left.next
+        right = right.next.next  
+
+    prev.next = left.next
+    return  header   
+
+
+he = removeMid(head)
+while he:
+    print(he.data,'-->',end="")
+    he = he.next 
+print('None')
+
+
 
 
 
