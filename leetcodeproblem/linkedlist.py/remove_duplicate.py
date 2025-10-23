b3e1168c0350cff74e4cb1c0530a63b7092b273c
syedmhamil <syedmahamil56@gmail.com>
2025-10-23 12:13:32 +0500

    dsa

--- leetcodeproblem/linkedlist.py/remove_duplicate.py
+++ leetcodeproblem/linkedlist.py/remove_duplicate.py
@@ -16,7 +16,7 @@ class Node:
 # for head1
 
 head = Node(1)    
-head.next = Node(2)    
-head.next.next = Node(3)    
-head.next.next.next = Node(4)  
-head.next.next.next.next = Node(5)  
+head.next = Node(1)    
+head.next.next = Node(2)    
+head.next.next.next = Node(3)  
+head.next.next.next.next = Node(3)  
