aa1aa867d9f0f2659d791c018887e6499db3122b
syedmhamil <syedmahamil56@gmail.com>
2025-10-24 10:34:27 +0500

    remove-dup

--- leetcodeproblem/linkedlist.py/remove_duplicate.py
+++ leetcodeproblem/linkedlist.py/remove_duplicate.py
@@ -20,3 +20,29 @@ head.next = Node(1)
 head.next.next = Node(2)    
 head.next.next.next = Node(3)  
 head.next.next.next.next = Node(3)  
+
+
+
+def removeDup(head):
+    if not head.next or not head:
+        return None
+    
+
+    curr = head
+    
+    while curr and curr.next:
+        if curr.data == curr.next.data:
+            curr.next = curr.next.next
+        else:
+            curr= curr.next
+    
+    return  head   
+
+
+he = removeDup(head)
+while he:
+    print(he.data,'-->',end="")
+    he = he.next
+
+print('None')
+
