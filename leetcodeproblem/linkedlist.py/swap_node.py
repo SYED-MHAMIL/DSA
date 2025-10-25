b405938c33dfbb10cf447c37ab03586780a42b75
syedmhamil <syedmahamil56@gmail.com>
2025-10-25 11:21:11 +0500

    dsa

--- leetcodeproblem/linkedlist.py/swap_node.py
+++ leetcodeproblem/linkedlist.py/swap_node.py
@@ -10,3 +10,48 @@
 
 
 
+class Node:
+    def __init__(self,data):
+        self.data = data 
+        self.next = None
+    
+# for head1
+
+head = Node(1)    
+head.next = Node(2)    
+head.next.next = Node(3)    
+head.next.next.next = Node(4)  
+# head.next.next.next.next = Node(5)  
+
+
+def swap_nodePair(header,n):
+    slow = header
+    fast = header.next
+    temp = None
+    while fast and fast.next:
+        temp = fast.next
+        temp2 = slow.next
+        fast.next = slow
+        slow.next = temp
+
+
+        slow = temp2.next 
+        fast = temp.next
+         
+
+
+    return  header
+
+findnode = swap_nodePair(head,1)
+while findnode:
+    print(findnode.data,'-->',end="")
+    findnode = findnode.next
+
+print('None')
+
+
+    
+    
+
+
+    
