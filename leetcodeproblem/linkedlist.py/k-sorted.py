b076b4dead86f15ec5d74b938caf49c530808616
syedmhamil <syedmahamil56@gmail.com>
2025-10-29 11:02:12 +0500

    ds

---
+++ leetcodeproblem/linkedlist.py/k-sorted.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self,data):
+        self.data = data 
+        self.next = None
+    
+# for head1
+
+head = Node(1)    
+# head.next = Node(2)    
+# head.next.next = Node(3)    
+# head.next.next.next = Node(4)  
+# head.next.next.next.next = Node(5)  
