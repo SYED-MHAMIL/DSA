47e4215f6dc04af6407047941ea98dda26ce32ec
syedmhamil <syedmahamil56@gmail.com>
2025-11-12 12:11:44 +0500

    add

--- leetcodeproblem/practise/remove.py
+++ leetcodeproblem/practise/remove.py
@@ -1,21 +1,21 @@
 def removeDuplicates(nums) :
-        # officernextroom = check_dup = 1
-        # n = len(nums)
-        # count =1
+        officernextroom = check_dup = 1
+        n = len(nums)
+        count =1
 
-        # if nums[0] == nums[check_dup]:
-        #    check_dup += 1 
+        if nums[0] == nums[check_dup]:
+           check_dup += 1 
 
-        # while check_dup < n :
+        while check_dup < n :
             
-        #     if  nums[officernextroom] == nums[check_dup]:
-        #         check_dup += 1
-        #     else:
-        #             nums[officernextroom] = nums[check_dup]
-        #             officernextroom+=1
-        #             check_dup+=1
-        #             count+=1
-        # return count,nums
+            if  nums[officernextroom] == nums[check_dup]:
+                check_dup += 1
+            else:
+                    nums[officernextroom] = nums[check_dup]
+                    officernextroom+=1
+                    check_dup+=1
+                    count+=1
+        return count,nums
         
         
         
