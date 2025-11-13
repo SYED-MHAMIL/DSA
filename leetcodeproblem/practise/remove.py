400d66e3e58c25ebf351d5d8a9ae395b679ac451
syedmhamil <syedmahamil56@gmail.com>
2025-11-13 11:42:51 +0500

    sd

--- leetcodeproblem/practise/remove.py
+++ leetcodeproblem/practise/remove.py
@@ -1,31 +1,31 @@
 def removeDuplicates(nums) :
-        officernextroom = check_dup = 1
-        n = len(nums)
-        count =1
+        # officernextroom = check_dup = 1
+        # n = len(nums)
+        # count =1
 
-        if nums[0] == nums[check_dup]:
-           check_dup += 1 
+        # if nums[0] == nums[check_dup]:
+        #    check_dup += 1 
 
-        while check_dup < n :
+        # while check_dup < n :
             
-            if  nums[officernextroom] == nums[check_dup]:
-                check_dup += 1
-            else:
-                    nums[officernextroom] = nums[check_dup]
-                    officernextroom+=1
-                    check_dup+=1
-                    count+=1
-        return count,nums
+        #     if  nums[officernextroom] == nums[check_dup]:
+        #         check_dup += 1
+        #     else:
+        #             nums[officernextroom] = nums[check_dup]
+        #             officernextroom+=1
+        #             check_dup+=1
+        #             count+=1
+        # return count,nums
         
         
-        
+                
         
         l=r=0
         n = len(nums)
         
         while r< n:
             nums[l] = nums[r]
-            [1,2,3,3]
+            # [1,2,3,3]
             while r < n  and  nums[l] == nums[r] :
                   r+=1
             l+=1  
---
+++ leetcodeproblem/practise/removedup2.py
@@ -0,0 +1,22 @@
+numbers = [0,1,1,1,4,4,4]
+
+def removeDup(nums) ->None:
+    
+    l, r = 0,0 
+
+    while r< len(nums):
+        count=1
+        while (r+1) < len(nums) and nums[r] == nums[r+1]:
+            r+=1
+            count+=1
+            
+        for i in range(min(2,count)):
+            nums[l] = nums[r]
+            l +=1
+        r+=1    
+        
+    return l,nums  
+
+
+        
+print(removeDup(numbers))
