104f85643ce5c90eb3ed4a21ba76d1b8162475c5
syedmhamil <syedmahamil56@gmail.com>
2025-11-14 11:38:26 +0500

    dsa

--- leetcodeproblem/practise/removedup2.py
+++ leetcodeproblem/practise/removedup2.py
@@ -24,3 +24,55 @@ def removeDup(nums,k=3) ->None:
         
         
 print(removeDup(numbers))
+
+
+
+
+
+# ==================================================================
+    #       for deleted list atmost  2 
+# ==================================================================
+
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        officernextroom = check_dup = 1
+        n = len(nums)
+        count =1
+
+        while check_dup < n :
+            
+            if  nums[check_dup-1] == nums[check_dup]:
+                check_dup += 1
+            else:
+                    nums[officernextroom] = nums[check_dup]
+                    officernextroom+=1
+                    check_dup+=1
+                    count+=1
+        return count  
+    
+    
+    
+    
+    
+
+
+
+
+
+def removeDup(nums,k=3) ->None:
+    l,r = 0,0 
+    
+    while r < len(nums):
+        count =1
+        while r+1 < len(nums) and  nums[r] == nums[r+1]:
+            count+=1
+            r+=1
+        for i in range(min(k,count)):
+            nums[l] = nums[r]
+            l+=1
+            
+        r+=1
+        
+    return l 
