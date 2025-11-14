fd195a34aed426cacd984c4f9e85bf959fe662a8
syedmhamil <syedmahamil56@gmail.com>
2025-11-14 11:35:45 +0500

    dsa

--- leetcodeproblem/practise/removedup2.py
+++ leetcodeproblem/practise/removedup2.py
@@ -1,22 +1,26 @@
-numbers = [0,1,1,1,4,4,4]
+# for k sorted
 
-def removeDup(nums) ->None:
+
+
+
+numbers = [0,0,1,1,1,4,4,4,4]
+
+def removeDup(nums,k=3) ->None:
+    l,r = 0,0 
     
-    l, r = 0,0 
-
-    while r< len(nums):
-        count=1
-        while (r+1) < len(nums) and nums[r] == nums[r+1]:
-            r+=1
+    while r < len(nums):
+        count =1
+        while r+1 < len(nums) and  nums[r] == nums[r+1]:
             count+=1
-            
-        for i in range(min(2,count)):
+            r+=1
+        for i in range(min(k,count)):
             nums[l] = nums[r]
-            l +=1
-        r+=1    
+            l+=1
+            
+        r+=1
         
-    return l,nums  
+    return l 
 
-
+        
         
 print(removeDup(numbers))
