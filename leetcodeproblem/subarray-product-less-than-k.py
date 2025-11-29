d51241cd8bbb349115a953eb4f4f6362e4e2451a
syedmhamil <syedmahamil56@gmail.com>
2025-11-29 12:40:45 +0500

    add

---
+++ leetcodeproblem/subarray-product-less-than-k.py
@@ -0,0 +1,8 @@
+# Given an array of integers nums and an integer k, return the number of contiguous subarrays where the product of all the elements in the subarray is strictly less than k.
+
+
+def numSubarrayProductLessThanK(nums,k):
+    for i in range(len(nums)):
+        for j in range()
+    
+    
