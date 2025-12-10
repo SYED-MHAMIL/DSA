fa280696b852513e113a9b547b3d3036312bd163
syedmhamil <syedmahamil56@gmail.com>
2025-12-10 13:14:32 +0500

    add

--- leetcodeproblem/shortest_unsorted.py
+++ leetcodeproblem/shortest_unsorted.py
@@ -1,9 +1,12 @@
+
 def findUnsortedSubarray(nums):
          
         l =  0  
         n = len(nums) 
-        # Input: nums = [2,6,4,8,10,9,15]
-    #    [1,2,3,4]
+    #  Input: nums = [2,6,4,8,10,9,15]
+    #  [1,3,2,2,2,4]
+    #  [1,2,3,4,3,5,6,5,3,6,7,3,6,7,8]
+
         while l < n-1 and nums[l] <= nums[l+1]:
             l+=1
         
@@ -12,7 +15,16 @@ def findUnsortedSubarray(nums):
         r =   n -1
         while r > 0 and nums[r] >= nums[r-1]:     
             r-=1
-        return r-l + 1        
-print(findUnsortedSubarray([1,2,3,4]))
-
+        
+        min_value = min(nums[l:r+1])            
+        max_value= max(nums[l:r+1])            
+      
+        while r < n-1 and nums[r+1] < max_value :
+            r+=1
+            
+        while l > 0  and nums[l-1] < min_value :
+            l-=1
+                    
+        return (r-l) + 1        
+print(findUnsortedSubarray([1,3,2,2,2,4]))
 
