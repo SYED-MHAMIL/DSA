fb00f0a5f2c2a60d8670e05f1ea9a9a0887ebf88
syedmhamil <syedmahamil56@gmail.com>
2025-12-20 12:58:52 +0500

    add

--- leetcodeproblem/sliding_Window/minimum-size-subarray-sum.py
+++ leetcodeproblem/sliding_Window/minimum-size-subarray-sum.py
@@ -21,20 +21,26 @@
 
 
 def minSubArraayLen(target,nums):
+# Input: target = 7, nums = [2,3,1,2,4,3]
+    
     res = float('inf')
     n= len(nums)
     l=0
     r=0
-    while r <= n and  l<= r :
-        value= sum(nums[l:r+1])
+    value = 0
+    
+    while r < n and  l<=r :
+        value+=nums[r]
         if(value < target):
              r+=1
         else:
-            print(nums[l:r+1])
-            res = min(res,r-l+1)
-            l+=1
+            # print(r-l+1)
+           while value >= target: 
+                res = min(res,r-l+1)
+                value-=nums[l]
+                l+=1
+           r+=1
             
-    
     return   0 if isinstance(res,float) else res
 
 print(minSubArraayLen(target = 7, nums = [2,3,1,2,4,3]))
