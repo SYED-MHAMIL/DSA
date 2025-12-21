e97b160d3188e8729fa71993f9d3acb93373e175
syedmhamil <syedmahamil56@gmail.com>
2025-12-21 11:10:10 +0500

    add

--- leetcodeproblem/sliding_Window/minimum-size-subarray-sum.py
+++ leetcodeproblem/sliding_Window/minimum-size-subarray-sum.py
@@ -1,6 +1,7 @@
 # Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
 
  
+            # print(r-l+1)
 
 # Example 1:
 
@@ -16,31 +17,26 @@
 # Input: target = 11, nums = [1,1,1,1,1,1,1,1]
 # Output: 0
  
-
 # Constraints:
 
 
 def minSubArraayLen(target,nums):
 # Input: target = 7, nums = [2,3,1,2,4,3]
-    
+
     res = float('inf')
     n= len(nums)
     l=0
-    r=0
-    value = 0
+    total = 0
+    for r in range(0,n):
+        total+=nums[r]
+        while total >= target:
+           res = min(res,r-l+1)
+           total-= nums[l]
+           l-=1
+             
     
-    while r < n and  l<=r :
-        value+=nums[r]
-        if(value < target):
-             r+=1
-        else:
-            # print(r-l+1)
-           while value >= target: 
-                res = min(res,r-l+1)
-                value-=nums[l]
-                l+=1
-           r+=1
+    
             
     return   0 if isinstance(res,float) else res
 
-print(minSubArraayLen(target = 7, nums = [2,3,1,2,4,3]))
+print(minSubArraayLen(target = 7, nums = [2,3,1,2,4,3]))    
