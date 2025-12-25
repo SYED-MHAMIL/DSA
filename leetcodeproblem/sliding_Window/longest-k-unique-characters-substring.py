f0236f3e1f88cb782148dbd386d3afec67935802
syedmhamil <syedmahamil56@gmail.com>
2025-12-25 12:36:53 +0500

    add

--- leetcodeproblem/sliding_Window/longest-k-unique-characters-substring.py
+++ leetcodeproblem/sliding_Window/longest-k-unique-characters-substring.py
@@ -21,4 +21,44 @@ def longestKSubstr(s, k):
                  
     return  -1 if (res < k) else  res
 
+# print(longestKSubstr("aabacbebebe",3))
+
+
+
+
+def longestKSubstr(s, k):
+        # code here
+    #   first step will be take out the length
+    #  second we will make the varibale  that take the longest substring
+    #  we will put it there loop  for 
+        n = len(s)
+        res = 0
+        obj={}
+        l= 0 
+        r=0 
+        
+        while r < n :
+            obj[s[r]] = obj.get(s[r],0) +1
+            print("obj",obj) 
+            if len(obj) == k:
+                res = max(res,r-l+1)
+                r+=1
+            elif len(obj) > k:
+                l+=1
+                if obj["a"] <= 1:            
+                   del obj["a"]
+                else:
+                    obj["a"] -= 1
+            else:
+                l+=1
+                if obj["a"] <= 1:            
+                   del obj["a"]
+                else:
+                    obj["a"] -= 1
+                            
+         
+               
+                
+        return  -1 if (res < k) else  res
+    
 print(longestKSubstr("aabacbebebe",3))
