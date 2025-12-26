2cb662f2f11f91d54f95abe38cf0a4b9f2adb15a
syedmhamil <syedmahamil56@gmail.com>
2025-12-26 13:02:30 +0500

    add

--- leetcodeproblem/sliding_Window/longest-k-unique-characters-substring.py
+++ leetcodeproblem/sliding_Window/longest-k-unique-characters-substring.py
@@ -1,27 +1,27 @@
 # You are given a string s consisting only lowercase alphabets and an integer k. Your task is to find the length of the longest substring that contains exactly k distinct characters.
 
 # Note : If no such substring exists, return -1. 
-def longestKSubstr(s, k):
-    # code here
-#   first step will be take out the length
-#  second we will make the varibale  that take the longest substring
-#  we will put it there loop  for 
-    n = len(s)
-    res = -1
+# def longestKSubstr(s, k):
+#     # code here
+# #   first step will be take out the length
+# #  second we will make the varibale  that take the longest substring
+# #  we will put it there loop  for 
+#     n = len(s)
+#     res = -1
     
-    for l in range(n):
-        obj={}
-        for r in range(l,n):
+#     for l in range(n):
+#         obj={}
+#         for r in range(l,n):
             
-            obj[s[r]] = obj.get(r,0)+1
-            if len(obj) == k:
-                res = max(res,r-l+1)
-            elif len(obj) > k:
-                break
+#             obj[s[r]] = obj.get(r,0)+1
+#             if len(obj) == k:
+#                 res = max(res,r-l+1)
+#             elif len(obj) > k:
+#                 break
                  
-    return  -1 if (res < k) else  res
+#     return  -1 if (res < k) else  res
 
-# print(longestKSubstr("aabacbebebe",3))
+# print(longestKSubstr("aaba{cbe]bebe",3))
 
 
 
@@ -31,6 +31,8 @@ def longestKSubstr(s, k):
     #   first step will be take out the length
     #  second we will make the varibale  that take the longest substring
     #  we will put it there loop  for 
+        # ebngqsoagtufqifwf
+        
         n = len(s)
         res = 0
         obj={}
@@ -39,26 +41,24 @@ def longestKSubstr(s, k):
         
         while r < n :
             obj[s[r]] = obj.get(s[r],0) +1
-            print("obj",obj) 
+            # print("obj",obj) 
             if len(obj) == k:
                 res = max(res,r-l+1)
                 r+=1
             elif len(obj) > k:
-                l+=1
-                if obj["a"] <= 1:            
-                   del obj["a"]
+              while not (len(obj) <= k):
+
+                print("index",l)  
+                if obj[s[l]] == 1:            
+                   del obj[s[l]]
                 else:
-                    obj["a"] -= 1
+                    obj[s[l]] -= 1
+                print(obj)
+                l+=1
+              r+=1
             else:
-                l+=1
-                if obj["a"] <= 1:            
-                   del obj["a"]
-                else:
-                    obj["a"] -= 1
-                            
-         
-               
+                r+=1              
                 
         return  -1 if (res < k) else  res
     
-print(longestKSubstr("aabacbebebe",3))
+print(longestKSubstr(s = "ebngqsoagtufqifwf", k = 5))
