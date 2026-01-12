fb4e4d31e4643d2464e4220c53622d7a831375c2
syedmhamil <syedmahamil56@gmail.com>
2026-01-12 11:51:27 +0500

    add

--- leetcodeproblem/sliding_Window/minimum-window-substring.py
+++ leetcodeproblem/sliding_Window/minimum-window-substring.py
@@ -1,26 +1,31 @@
 def minWindow( s, t) :
         n = len(s)
         left = 0
-        res= []
+        res= s.split()
         obj = {}
         for i in t:
            obj[i] = obj.get(i,0) + 1 
 
         for right in range(n):
-            res.append(s[right])
+            
             if s[right] in obj:
                if obj[s[right]] > 1:
                    obj[s[right]] -=1
                else:
                    del obj[s[right]]
-
+            
+            print("element" ,s[right])
+            
+            
             while len(obj)  == 0:
                res=min(res,list(s[left:right+1]))
-               res.pop(left)
-               
+               print("Res", res)
+               if s[left] in t:
+                   obj[s[left]] = obj.get(s[left],0) + 1  
+               print("obj fareq" ,obj )
+                   
+               print("left pointer" ,left )
                left+=1 
                
-            print("heloo lrft", left )
-               
-               
+        return res     
 print(minWindow("ADOBECODEBANC" ,"ABC"))
