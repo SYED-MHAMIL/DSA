8126a6aa2ac40b89b6e3c8593f141953c8bc4891
syedmhamil <syedmahamil56@gmail.com>
2026-01-15 11:38:06 +0500

    add

--- leetcodeproblem/sliding_Window/minimum-window-substring.py
+++ leetcodeproblem/sliding_Window/minimum-window-substring.py
@@ -1,31 +1,29 @@
 def minWindow( s, t) :
         n = len(s)
         left = 0
-        res= s.split()
-        obj = {}
+        res= float('inf')
+        requirement = {}
+        need = {}
         for i in t:
-           obj[i] = obj.get(i,0) + 1 
+           requirement[i] = requirement.get(i,0) + 1 
 
         for right in range(n):
             
-            if s[right] in obj:
-               if obj[s[right]] > 1:
-                   obj[s[right]] -=1
-               else:
-                   del obj[s[right]]
+            if s[right] in requirement:
+               need[s[right]] =  need.get(s[right],0)+1
+               
             
-            print("element" ,s[right])
-            
-            
-            while len(obj)  == 0:
-               res=min(res,list(s[left:right+1]))
-               print("Res", res)
-            #    if s[left] in t:
-            #        obj[s[left]] = obj.get(s[left],0) + 1  
-            #    print("obj fareq" ,obj )
-                   
-            #    print("left pointer" ,left )
+            while isRequirementFull:=  all( need.get(c,0) >= requirement[c] for c in  requirement):
+               res=min(res, right-left+1)
+            #    print("Res", res)
+            #    print("subs array", list(s[left:right+1]))
+               
+               if s[left] in requirement:
+                   if need.get(s[left],0) > 1:
+                      need[s[left]]-=1
+                   else:
+                        del need[s[left]]  
                left+=1 
                
-        return res     
-print(minWindow("ADOBECODEBANC" ,"ABC"))
+        return "" if isinstance(res,float) else res   
+print(minWindow("a" ,"a"))  
