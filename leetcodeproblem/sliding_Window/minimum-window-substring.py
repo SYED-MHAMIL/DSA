8efe4e24695d57c0a2ca1dc2f4de4603daee6455
syedmhamil <syedmahamil56@gmail.com>
2026-01-16 12:46:14 +0500

    done-minwindow

--- leetcodeproblem/sliding_Window/minimum-window-substring.py
+++ leetcodeproblem/sliding_Window/minimum-window-substring.py
@@ -2,6 +2,7 @@ def minWindow( s, t) :
         n = len(s)
         left = 0
         res= float('inf')
+        startingIndex = 0
         requirement = {}
         need = {}
         for i in t:
@@ -12,7 +13,9 @@ def minWindow( s, t) :
                need[s[right]] =  need.get(s[right],0)+1
                  
             while isRequirementFull:=  all( need.get(c,0) >= requirement[c] for c in  requirement):
-               res=min(res, right-left+1)
+               if res > right-left+1:
+                  startingIndex = left
+               res=min(res,right-left+1)
                if s[left] in requirement:
                    if need.get(s[left],0) > 1:
                       need[s[left]]-=1
@@ -20,5 +23,5 @@ def minWindow( s, t) :
                         del need[s[left]]  
                left+=1 
                
-        return "" if isinstance(res,float) else res   
-print(minWindow( "ADOBECODEBANC", t = "ABC"))  
+        return "" if isinstance(res,float) else s[startingIndex:startingIndex+res]   
+print(minWindow( "a", t = "aa")) 
