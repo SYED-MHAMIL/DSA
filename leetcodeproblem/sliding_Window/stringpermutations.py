4b74a3a348bdb3093f077c1dffe069935d0cfae8
syedmhamil <syedmahamil56@gmail.com>
2026-01-18 12:42:27 +0500

    add

--- leetcodeproblem/sliding_Window/stringpermutations.py
+++ leetcodeproblem/sliding_Window/stringpermutations.py
@@ -1,18 +1,23 @@
-from itertools import permutations
-class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
-        permutation =  []
-        for per in permutations(s1):
-            permutation.append("".join(per))
+from collections import Counter
+
+def checkInclusion( s1: str, s2: str) -> bool:
+        permutation =  {}
+        for per in s1:
+            permutation[per] =permutation.get(per,0) + 1 
+        
         
         left = 0 
         right =  len(s1)
         n= len(s2)
         while right <= n:
-             isContains = s2[left:right]
-             if isContains in permutation:
+             isContains = Counter(s2[left:right])
+             print({"is conte" : isContains, "permuta" : permutation})   
+
+             if isContains ==  permutation:
                 return True
              left+=1
              right+=1
 
         return False
+    
+print(checkInclusion(s1 = "ab", s2 = "eidbaooo"))
