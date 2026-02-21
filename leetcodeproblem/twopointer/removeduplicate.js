21ac4edaecd84de11858edfdb3c42aa99c21000b
syedmhamil <syedmahamil56@gmail.com>
2026-02-21 12:24:50 +0500

    add

--- leetcodeproblem/twopointer/removeduplicate.js
+++ leetcodeproblem/twopointer/removeduplicate.js
@@ -41,15 +41,29 @@
 
 
 var removeElement = function(nums, val) {
-   nums.sort()
+   nums.sort((a, b) => a - b);
+
    let left= 0
    let right = nums.length -1
    let k =0 
-   while (left  < right){
+   // [0,0,1,2,2,2,3,4] , val =2 
+   // [0,0,1,4,2,2,3,2]
+   // [0,0,1,4,3,2,2,2]
+
+   // if (nums.length == 1 &  nums[0] == val) {
+   //    return nums.length
+   // }
+
+   while (left  <= right){
+      
+      console.log('left',left);
+      console.log('right',right);
         if(nums[left] === val){
-           let temp = nums[left];
-nums[left] = nums[right];
-nums[right] = temp;
+           if(nums[left] !== nums[right]){
+            k+=1
+           }
+            [nums[left], nums[right]] = [nums[right], nums[left]];
+
              left+=1
              right-=1
         }else{
@@ -57,9 +71,9 @@ nums[right] = temp;
             left+=1
         }
    }
-   console.log(nums);
+
    
    return  k
 };
 
-console.log(removeElement(nums = [0,1,2,2,3,0,4,2], val = 3))
+console.log(removeElement(nums = [0,1,2,2,3,0,4,2], val = 2))
