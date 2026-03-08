24e049e2e8a7f47d2e3f9ee7dc9cb0c85fced23e
syedmhamil <syedmahamil56@gmail.com>
2026-03-08 12:16:51 +0500

    add

--- leetcodeproblem/twopointer/next.permutation.js
+++ leetcodeproblem/twopointer/next.permutation.js
@@ -1,25 +1,35 @@
 function nextpermutation(arr) {
     let i= arr.length - 2 
-    let j= arr.length-1
     let c=arr.length-1
     
-    while(i <= 0){
-       if(arr[i] < arr[i+1]){
+    while(i >= 0){
         // first get decrasing ele from right
+        if(arr[i] < arr[i+1]){
+         console.log(i)
           let first_dec_el = arr[i]
           for(let k=c; k >=0; k--){
-              if(arr[c] > arr[i]){
-                  [arr[i],arr[c]]=[arr[c],arr[i]]
-                  if (arr[arr.length -2] <arr[arr.length -1]) {
-                    
-                  }
+                 
+              if(arr[k] > arr[i]){                
+                  [arr[i],arr[k]]=[arr[k],arr[i]]
+                  break
               }
           }
+          let l=i+1
+          let r= arr.length -1
+          while (l < r){
+              [arr[r],arr[l]]  = [arr[l],arr[r]] 
+              l+=1
+              r-=1
+          }
           
-
-
+       return arr
+        
        }
+       i-=1
     }
-
+    return arr.reverse()
 
 }
+
+
+console.log(nextpermutation([2,3,1]));
