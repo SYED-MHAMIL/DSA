62f374bf5aa5b342f184d0ee462cd8e093ad5ddf
syedmhamil <syedmahamil56@gmail.com>
2026-03-18 14:00:55 +0500

    valid-parentheses

---
+++ leetcodeproblem/stack/valid-paren.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = []
+    let open  = {
+        ")" :"(",
+         "}" : "{",
+         "]": "{"
+    } 
+    for(c of s){
+        if(open[c]){
+            
+            if(open[c] == stack[stack.length -1]){
+                stack.pop()      
+            }else{
+                return false
+            }
+        }else{
+            stack.push(c)
+        }
+
+    }
+    return stack.length == 0  
+};
+
+
+console.log(isValid("()[]{}"
+
+));
