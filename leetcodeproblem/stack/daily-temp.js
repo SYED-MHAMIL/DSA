6d3745a1f85c3f6965402ab3b8b1d1c82add71a4
SYED-MHAMIL <syedmahamil56@gmail.com>
2026-03-20 12:55:31 -0700

    add

---
+++ leetcodeproblem/stack/daily-temp.js
@@ -0,0 +1,51 @@
+//  see in the solution we can get the answer from the question as well so lets go through with solution 
+// /**
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+// var nextGreaterElements = function(nums) {
+//     let stack = []
+//     let res = new Array(nums.length).fill(-1) 
+//     for(let i=0; i<num.length; i++){
+//     while(stack && nums[stack[stack.length -1]] < nums[i]){
+//          const pop_el = stack.pop()
+//          res[pop_el]=nums[i]
+//     }
+//     stack.push(i)
+//     }
+//      if(stack){
+//        res[]
+//      }
+    
+// };
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var nextGreaterElements = function(nums) {
+    let stack = []
+    let res = new Array(nums.length).fill(-1)
+    let n= nums.length 
+ 
+    for(let i=0; i<n*2; i++){
+        console.log(i)
+    while(stack && nums[stack[stack.length -1]] < nums[i%n]){
+         const pop_el = stack.pop()
+         console.log("opo",pop_el)
+         res[pop_el]=nums[i%n]
+    }
+
+    if(i < n){
+     stack.push(i%n)
+    }
+   
+    }
+
+   return res   
+    
+};
+
+console.log(nextGreaterElements([1,2,1]))
+
+
